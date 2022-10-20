759b798afeeebba721ec847057386f951f78f288
unoexp <44316768+unoexp@users.noreply.github.com>
2022-10-20 19:30:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,25 +4,61 @@
 import get_my_friends
 import get_moods
 import get_qq_number
-
-
-if __name__ == '__main__':
-
-    # 先获取包含好友QQ号码的文件
-    get_friends_obj = get_my_friends.Get_friends_number()
-    get_friends_obj.get_friends()
-
-    # Second, deal with this data, clean it
-    # From the get_friends result get the useful data
-    # And save it to file qqnumber.inc
-    # The format of this file just a list
-    # 然后，从这些文件里面提取出QQ号码
+import get_moods_detail
+import requests
+def main():
+    print('准备工作')
     get_qq_item_obj = get_qq_number.exact_data_from_result()
     get_qq_item_obj.exact_qq_number()
-
-    # Finally, use the cleaned data to get mood
-    # Base on last step's qqnumber.inc file
-    # exact the qq number and start to get their moods
-    # 最后，根据每一个QQ号码去获取对应的动态
-    get_moods_obj = get_moods.Get_moods_start()
-    get_moods_obj.get_moods_start()
+    print('欢迎！')
+    a=input('\n输入...\n1：收集所有人（并重新获取好友列表）\n2：收集所有人（不重新获取好友列表）\n3：获取单人\n')
+    while(True):
+        
+        if a=='1':#所有人 with friends
+            get_friends_obj = get_my_friends.Get_friends_number()
+            get_friends_obj.get_friends()
+            get_qq_item_obj = get_qq_number.exact_data_from_result()
+            get_qq_item_obj.exact_qq_number()
+            get_moods_obj = get_moods.Get_moods_start(0)
+            get_moods_obj.get_moods_start(0)
+            break
+        elif a=='2':#所有人 without friends
+            get_qq_item_obj = get_qq_number.exact_data_from_result()
+            get_qq_item_obj.exact_qq_number()
+            get_moods_obj = get_moods.Get_moods_start(0)
+            get_moods_obj.get_moods_start(0)
+            break
+        elif a=='3':#单人
+            b= input('\n输入id\n')
+            get_moods_obj=get_moods.Get_moods_start(str(b))
+            get_moods_obj.get_moods_start(1)
+            break
+        elif a=='4': 
+#            将number替换为自定义号码            
+            gt('number') #输入自定义号码
+            #gt('...')
+            
+            print('收集完成')
+            return
+        elif a=='0': 
+            gt('number')#自定义
+            return
+        elif a=='.':
+            com('number')
+        else:
+            a=input('你输'+str(a)+'干什么呢？？\n重新输')
+            
+    c= input('\n收集完成！\n\n是否整理？\n1：整理\n2：不整理\n')
+    if c == '1':
+        paa=get_moods_detail.main()
+    else:
+        print('如果想单独整理可运行get_moods_detail.py')
+        
+def gt(num):
+    get_moods_obj=get_moods.Get_moods_start(num)
+    get_moods_obj.get_moods_start(1)
+    paa=get_moods_detail.ddd(num)
+def com(num):
+    ge=get_moods.Get_moods(num)
+    ge.compare()
+main()

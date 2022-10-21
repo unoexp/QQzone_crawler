e78f8efa554f51df60a34714a959370720f6cfaf
unoexp <44316768+unoexp@users.noreply.github.com>
2022-10-21 19:35:10 +0800

    Update util.py

--- util.py
+++ util.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-#-*- coding:utf-8 -*-
-
-'''
-用到的工具函数、变量集合
-'''
-
 import os
 import re
 from urllib import parse
@@ -12,53 +5,49 @@ import requests
 
 
 def get_cookie():
-    '''Get cookie from cookie_file'''
     with open('cookie_file') as f:
         cookie = f.read()
     cookie = cookie.replace('\n', '')
-
     return cookie
+
 
 cookie = get_cookie()
 
 headers = {'host': 'h5.qzone.qq.com',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-            'Accept-Language': 'zh,zh-CN;q=0.8,en-US;q=0.5,en;q=0.3',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Cookie': cookie,
-            'connection': 'keep-alive'}
-hea= {'host': 'user.qzone.qq.com',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-            'Accept-Language': 'zh,zh-CN;q=0.8,en-US;q=0.5,en;q=0.3',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Cookie': cookie,
-            'connection': 'keep-alive'}
+           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+           'Accept-Language': 'zh,zh-CN;q=0.8,en-US;q=0.5,en;q=0.3',
+           'Accept-Encoding': 'gzip, deflate, br',
+           'Cookie': cookie,
+           'connection': 'keep-alive'}
+hea = {'host': 'user.qzone.qq.com',
+       'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Language': 'zh,zh-CN;q=0.8,en-US;q=0.5,en;q=0.3',
+       'Accept-Encoding': 'gzip, deflate, br',
+       'Cookie': cookie,
+       'connection': 'keep-alive'}
+
+
 def get_g_tk():
-    ''' make g_tk value'''
-    with open ('cookie_file') as f:
-        tex=f.read()
+    with open('cookie_file') as f:
+        tex = f.read()
     if 'p_skey=' in tex:
-        
         pskey_start = cookie.find('p_skey=')
         pskey_end = cookie.find(';', pskey_start)
-        p_skey = cookie[pskey_start+7: pskey_end]
-    
+        p_skey = cookie[pskey_start + 7: pskey_end]
         h = 5381
-    
         for s in p_skey:
             h += (h << 5) + ord(s)
-    
         return h & 2147483647
-    else :
+    else:
         print('请检查cookie文件是否正确')
+
 
 g_tk = get_g_tk()
 
 
 def get_qzonetoken(qqnum):
-    '''获取qzonetoken，它位于空间首页的源代码中'''
     index_url = "https://user.qzone.qq.com/%s" % qqnum
     res = requests.get(index_url, headers=hea)
     src = res.text
@@ -67,10 +56,6 @@ def get_qzonetoken(qqnum):
 
 
 def parse_moods_url(qqnum):
-    '''This method use to get every friend's mood cgi url
-       So it needs the friend's qqnumber to get their url
-    '''
-
     params = {"cgi_host": "http://taotao.qq.com/cgi-bin/emotion_cgi_msglist_v6",
               "code_version": 1,
               "format": "jsonp",
@@ -86,24 +71,19 @@ def parse_moods_url(qqnum):
     host = "https://h5.qzone.qq.com/proxy/domain/taotao.qq.com/cgi-bin/emotion_cgi_msglist_v6?"
 
     url = host + parse.urlencode(params)
-    
     return url
 
-def parse_friends_url():
-    '''This method only generate the friends of the owner
-       So do not need to get qq number, just get it from
-       self cookie
-    '''
 
+def parse_friends_url():
     cookie = headers['Cookie']
     qq_start = cookie.find('uin=o')
     qq_end = cookie.find(';', qq_start)
-    qqnumber = cookie[qq_start+5 : qq_end]
-    if qqnumber[0] == 0:
-        qqnumber = qqnumber[1:]
+    qq_num = cookie[qq_start + 5: qq_end]
+    if qq_num[0] == 0:
+        qq_num = qq_num[1:]
     # 先获取qzonetoken
-    qzonetoken = get_qzonetoken(qqnumber)
-    params = {"uin": qqnumber,
+    qzonetoken = get_qzonetoken(qq_num)
+    params = {"uin": qq_num,
               "fupdate": 1,
               "action": 1,
               "g_tk": g_tk,
@@ -111,29 +91,24 @@ def parse_friends_url():
 
     host = "https://h5.qzone.qq.com/proxy/domain/base.qzone.qq.com/cgi-bin/right/get_entryuinlist.cgi?"
     url = host + parse.urlencode(params)
-    print(url)
     return url
 
-def parse_visitor(qq):
-    '''This method only generate the friends of the owner
-       So do not need to get qq number, just get it from
-       self cookie
-    '''
 
+def parse_visitor(qq):
     cookie = headers['Cookie']
     qq_start = cookie.find('uin=o')
     qq_end = cookie.find(';', qq_start)
-    qqnumber = cookie[qq_start+5 : qq_end]
+    qqnumber = cookie[qq_start + 5: qq_end]
     if qqnumber[0] == 0:
         qqnumber = qqnumber[1:]
     # 先获取qzonetoken
     qzonetoken = get_qzonetoken(qqnumber)
     params = {"uin": qq,
-              'mask':2,
+              'mask': 2,
               "g_tk": g_tk,
-              'page':1,
+              'page': 1,
               "fupdate": 1,
-              
+
               "qzonetoken": qzonetoken}
 
     host = "https://h5.qzone.qq.com/proxy/domain/g.qzone.qq.com/cgi-bin/friendshow/cgi_get_visitor_simple?"
@@ -141,10 +116,7 @@ def parse_visitor(qq):
     print(url)
     return url
 
-def check_path(path):
-    '''This method use to check if the path is exists.
-       If not, create that
-    '''
 
+def check_path(path):
     if not os.path.exists(path):
         os.makedirs(path)

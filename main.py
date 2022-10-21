9e3bc2b18ef4177b43e1a9d066c69c32b666c1ab
unoexp <44316768+unoexp@users.noreply.github.com>
2022-10-21 22:41:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
 import os
-
 import get_moods
 import get_moods_detail
 import get_my_friends
 import get_qq_number
 import util
+import cookie
 
 
 def main():
-    print('准备工作')
+    need_QR = input('欢迎使用！\n 如果您需要使用二维码登录获取cookie, 请输入1\n 如果您已经手动获取cookie, 请输入其他字符以继续\n')
+    if need_QR == '1':
+        cookie.get_cookie()
+
+    # 计算cookie和其他必要参数
+    util.calc_cookie()
+
+    # 建立所需文件夹
     util.check_path(os.path.join('friends'))
     util.check_path(os.path.join('content', 'all'))
     util.check_path(os.path.join('content', 'single'))
@@ -19,9 +24,10 @@ def main():
     util.check_path(os.path.join('result', 'all_html'))
     util.check_path(os.path.join('result', 'single_html'))
 
+    # 获取friends文件夹内保存的qq号码 并保存为qqnumber.inc
     get_qq_item_obj = get_qq_number
     get_qq_item_obj.exact_qq_number()
-    print('欢迎使用！')
+
     a = input('输入...\n1：收集所有人（同时重新获取好友列表）\n2：收集所有人（不重新获取好友列表）\n3：获取单人\n')
     while True:
         if a == '1':  # 所有人 同时重新获取好友列表
@@ -51,7 +57,7 @@ def main():
             break
         else:
             a = input('输入错误, 请重新输入')
-    print('如有需要可运行get_moods_detail.py单独整理某人/某天的')
+    print('\n如有需要可运行get_moods_detail.py单独整理某人/某天的')
 
 
 if __name__ == '__main__':

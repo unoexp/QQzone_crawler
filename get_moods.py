28549d889eed8287b1306a58221460604651e0cc
unoexp <44316768+unoexp@users.noreply.github.com>
2022-10-21 19:33:02 +0800

    Update get_moods.py

--- get_moods.py
+++ get_moods.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-#-*- coding:utf-8 -*-
-
-"""
-把包含动态的文件下载下来
-"""
-
-
 import requests
 import os
 import sys
@@ -13,183 +5,96 @@ import time
 import util
 import datetime
 
-class Get_moods(object):
-    '''Get moods file with cookie'''
 
-    def __init__(self,qqi):
+class Get_moods(object):
+    def __init__(self):
         self.session = requests.Session()
         self.headers = util.headers
         self.g_tk = util.g_tk
-        self.qq=qqi
-        self.timen=str(datetime.datetime.now())[:10]
-    def single(self):
-        qqnumber=self.qq
-        referer = 'http://user.qzone.qq.com/' + qqnumber
+        self.timen = str(datetime.datetime.now())[:10]
+
+    def get_moods(self, s, qq_num):
+        referer = 'http://user.qzone.qq.com/' + qq_num
         self.headers['Referer'] = referer
-        util.check_path('s/'+self.timen+'/'+qqnumber+'/')
-        url_base = util.parse_moods_url(qqnumber)
-        pos = 0
-        key = True
-        
-        while key:
-            print("\t正在获取 第:\t%d" % ((pos+20)//20)+'页')
-            url = url_base + "&pos=%d" % pos
-            res = self.session.get(url, headers = self.headers)
-            con = res.text
-            with open('s/'+self.timen+'/'+ qqnumber + '/' + str(pos), 'w', encoding="utf-8") as f:
-                
-                f.write(con)
-                if os.path.getsize('s/'+self.timen+'/'+ qqnumber + '/' + str(pos))/1024<2:
-                    
-                    break
-                
-            if '''"msglist":null''' in con:
-                key = False
-            
-            pos += 20
-            time.sleep(1)
-    def compare(self):
-        qqnumber=self.qq
-        referer = 'http://user.qzone.qq.com/' + qqnumber
-        self.headers['Referer'] = referer
-        util.check_path('s/'+self.timen+'/'+qqnumber+'/')
-        url_base = util.parse_moods_url(qqnumber)
-        pos = 0
-        key = True
-        url = url_base + "&pos=%d" % pos
-        res = self.session.get(url, headers = self.headers)
-        con = res.text
-        qaq=0
-        tm=0
-        while True:
-            
-            with open('s/'+self.timen+'/'+ qqnumber + '/' + str(pos), 'r', encoding="utf-8") as f:
-                co=f.read()
-                if not con==co:
-                    util.check_path('s/'+self.timen+'/'+ qqnumber + '/10/')
-                    with open('s/'+self.timen+'/'+ qqnumber + '/10/' + str(qaq), 'w', encoding="utf-8") as f:    
-                        f.write(con)
-                        qaq+=1
-                        with open('s/'+self.timen+'/'+ qqnumber + '/' + str(pos), 'w', encoding="utf-8") as f:
-                            f.write(con)
-                        print('替换一次!!!!!!!')
-                        pygame.mixer.init()
-                        
-                        track = pygame.mixer.music.load("dontmove/bing.mp3")
-                        pygame.mixer.music.play() 
+        if s == 'single':
+            path = os.path.join('content', 'single', self.timen, qq_num)
+        elif s == 'all':
+            path = os.path.join('content', 'all', self.timen, qq_num)
+        else:
+            return
 
-            tm+=1
-            print('已检查'+str(tm)+'次')
-            time.sleep(60) #检查周期
-            
-            
-    def get_moods(self,qqnumber):
-        '''Use cookie and header to get moods file and save it to result folder with QQnumber name'''
-        
-        referer = 'http://user.qzone.qq.com/' + qqnumber
-        self.headers['Referer'] = referer
-
-        # Create a folder with qq number to save it's result file
-        util.check_path('al/'+self.timen+'/' +qqnumber)
-
-        # Get the goal url, except the position argument.
-        url_base = util.parse_moods_url(qqnumber)
-
+        util.check_path(path)
+        url_base = util.parse_moods_url(qq_num)
         pos = 0
         key = True
 
         while key:
-            print("\t正在获取 第:\t%d" % ((pos+20)//20)+'页')
+            print(f'\t正在获取 第:\t{(pos + 20) // 20}页')
             url = url_base + "&pos=%d" % pos
-            # print(url)   # for debug use
-            res = self.session.get(url, headers = self.headers)
+            res = self.session.get(url, headers=self.headers)
             con = res.text
-            with open('al/'+self.timen+'/'+ qqnumber + '/' + str(pos), 'w', encoding="utf-8") as f:
+            with open(os.path.join(path, str(pos)), 'w', encoding="utf-8") as f:
                 f.write(con)
 
-            if '''"msglist":null''' in con:
+            if '"msglist":null' in con:
                 key = False
 
-            # Cannot access...
-            if '''"msgnum":0''' in con:
-                with open('crawler_log.log', 'a', encoding="utf-8") as log_file:
-                    log_file.write("%s Cannot access..\n" % qqnumber)
+            if '"subcode":2' in con:
+                if s == 'single':
+                    exit('对不起,主人设置了保密,您没有权限查看')
+                else:
+                    print(f'无法访问{qq_num}的空间')
                 key = False
 
-            # Cookie expried
-            if '''"subcode":-4001''' in con:
-                with open('crawler_log.log', 'a', encoding="utf-8") as log_file:
-                    log_file.write('Cookie Expried! Time is %s\n' % time.ctime())
-                sys.exit()
+            if '"subcode":-10000' in con:
+                print('获取中断, 使用人数过多，请稍后再试')
+                key = False
+
+            if '"msgnum":0' in con:
+                key = False
+
+            if '"subcode":-4001' in con:
+                exit(f'cookie有误{time.ctime()}\n')
 
             pos += 20
+            # 防ban
             time.sleep(1)
-
-    #below method only make for me to get the friend's mood
-    #which havn't download yet.
-    #
-    #def get_rest_number(self):
-
-    #    exists_number = os.listdir('mood_result')
-    #    with open('qqnumber_backup.inc') as f:
-    #        con = f.read()
-    #    con = eval(con)
-    #    for item in con:
-    #        qq = item['data']
-    #        if qq not in exists_number:
-    #            print("Dealing with:\t%s" % qq)
-    #            self.get_moods(qq)
-    #    else:
-    #        print('Finish!')
 
 
 class Get_moods_start(object):
 
-    def __init__(self,qqi):
+    def __init__(self, qqi):
         print('开始获取id')
-        self.qq=qqi
-        self.timen=str(datetime.datetime.now())[:10]
-    def get_moods_start(self,a):
-        app = Get_moods(self.qq)
-        #app.get_rest_number()
+        self.qq = qqi
+        self.timen = str(datetime.datetime.now())[:10]
+
+    def get_moods_start(self, a):
+        app = Get_moods()
 
         with open('qqnumber.inc', encoding="utf-8") as qnumber_file:
             qnumber_string = qnumber_file.read()
         qnumber_list = eval(qnumber_string)
 
-        # check if there is a mood_result folder to save the result file
-        # if not create it
-        util.check_path('s/'+self.timen+'/'+self.qq+'/' if a==1 else 'al/'+self.timen+'/')
-        if a==1 :
-            app.single()
+        if a == 1:
+            util.check_path(os.path.join('content', 'single', self.timen, self.qq))
+        else:
+            util.check_path(os.path.join('content', 'all', self.timen))
+
+        if a == 1:
+            app.get_moods('single', self.qq)
             print('完成')
         else:
-            
             while qnumber_list != []:
                 save_back_qnumber = qnumber_list[:]
                 item = qnumber_list.pop()
-                qq = item['data']
-                print("正在处理:\t%s" % qq)
-    
-                start_time = time.ctime()
-                with open('crawler_log.log', 'a', encoding="utf-8") as log_file:
-                    log_file.write("Program run at: %s\tGetting %s data...\n" % (start_time, qq))
-    
+                self.qq = item['data']
+                print(f'正在处理:\t{self.qq}')
                 try:
-                    app.get_moods(qq)
-                except KeyboardInterrupt:
-                    print('User Interrupt, program will exit')
-                    sys.exit()
+                    app.get_moods('all', self.qq)
                 except Exception as e:
-                    # Write the rest item back to qqnumber.inc
                     with open('qqnumber.inc', 'w', encoding="utf-8") as qnumber_file:
                         qnumber_file.write(str(save_back_qnumber))
-    
-                    # Write the log
-                    with open('crawler_log.log', 'a', encoding="utf-8") as log_file:
-                        exception_time = time.ctime()
-                        log_file.write("Exception occured: %s\n%s\n" % (exception_time, e))
                 else:
-                    print("%s 已完成" % qq)
+                    print(f'{self.qq} 已完成')
             else:
                 print("全部完成")
